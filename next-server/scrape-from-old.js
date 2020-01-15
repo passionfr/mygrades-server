20eeefbe4843957f00f83954186a89a1cb970aa0
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-01-16 00:44:12 +0100

    make api static

---
+++ next-server/scrape-from-old.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var Crawler = require("crawler");
+
+var c = new Crawler({
+    encoding: null,
+    jQuery: false,// set false to suppress warning message.
+    callback: function (err, res, done) {
+        if (err) {
+            console.error(err.stack);
+        } else {
+            if (res.options.level === "src") {
+                fs.createWriteStream(res.options.filename).write(res.body);
+                JSON.parse(res.body).forEach(uni => {
+                    c.queue({
+                        uri: "http://mygrades.dev/api/v1/universities/" + uni.university_id + "?detailed=true",
+                        filename: uni.university_id + ".json",
+                        level: "src/universities"
+                    })
+                })
+            } else {
+                fs.createWriteStream("./" + res.options.level + "/" + res.options.filename).write(res.body);
+
+            }
+        }
+
+        done();
+    },
+    userAgent: "test"
+});
+
+c.queue({
+    uri: "http://mygrades.dev/api/v1/universities",
+    filename: "universities.json",
+    level: "src"
+});
