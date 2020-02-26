6cdf55cff105d9e74d60c747edab0b2dff954083
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-02-26 23:27:30 +0100

    copy pages from old frontend

--- next-frontend/mygradesreloaded/src/app/app.module.ts
+++ next-frontend/mygradesreloaded/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataPrivacyComponent } from './data-privacy/data-privacy.component';
+import { MainComponent } from './main/main.component';
+import { SiteNoticeComponent } from './site-notice/site-notice.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataPrivacyComponent
+    DataPrivacyComponent,
+    MainComponent,
+    SiteNoticeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
